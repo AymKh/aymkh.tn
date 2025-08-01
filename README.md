562f51328a4333f8414150addc8b0d0877e0ac80
Aymen KHEDHRIYA <aymen.khedhriya@gmail.com>
2025-08-01 19:30:00 +0100

    chore: hard reset

--- README.md
+++
@@ -1,3 +0,0 @@
-# Personal Portfolio
-
-This repository houses the codebase for my own website hosted on [https://aymkh.tn](https://aymkh.tn)
--- src/types/projects.ts
+++
@@ -1,6 +0,0 @@
-export type TProject = {
-    project_name: string
-    project_description: string
-    project_demo_link?: string
-    project_on_github?: string
-}
